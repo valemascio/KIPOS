eeb6821a6cceba8b041883d8c934655e10ee054a
elenma98 <93485913+elenma98@users.noreply.github.com>
2022-04-06 19:20:35 +0200

    fixed Bage page

--- kipos/lib/screens/badgePage.dart
+++ kipos/lib/screens/badgePage.dart
@@ -3,12 +3,25 @@ import 'package:flutter/material.dart';
 class BadgePage extends StatelessWidget {
   const BadgePage({Key? key}) : super(key: key);
 
-  static const route = '/home/badge/';
-  static const routename = 'BadgePage';
+  static const route = '/badge/';
+  static const routename = 'badgePage';
 
   @override
   Widget build(BuildContext context) {
-    print('ciao');
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: (const Text('Bagets')),
+        backgroundColor: const Color.fromARGB(255, 34, 175, 34),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
-} //EditProfilePage
+}
