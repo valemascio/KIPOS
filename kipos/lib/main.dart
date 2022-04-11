346c0277b5ee5b19bc5f3b3cc4521d461f0b42da
elenma98 <93485913+elenma98@users.noreply.github.com>
2022-04-11 15:28:22 +0200

    change of text style

--- kipos/lib/main.dart
+++ kipos/lib/main.dart
@@ -14,16 +14,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    List<String> descriptions = [
-      'Testo tip 1',
-      'Testo tip 2',
-      'Testo tip 3',
+    List<Text> title = [
+      const Text(
+        'Choose the right pair of shoes',
+      ),
+      const Text(
+        'How to do stretching',
+      ),
+      const Text('Run with a pace of 80-100bpm')
     ];
 
-    List<String> title = [
-      'Choose the right pair of shoes',
-      'How to do stretching',
-      'Run with a pace of 80-100bpm'
+    List<Text> descriptions = [
+      const Text('Choose the right pair of shoes',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20)),
+      const Text('How to do stretching',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20)),
+      const Text('Run with a pace of 80-100bpm',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20))
     ];
 
     List<IconData> icon = [
