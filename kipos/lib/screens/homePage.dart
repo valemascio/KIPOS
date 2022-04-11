ff03cdfe0f22d967189d594d326f994aa4841697
valemascio <mascio.vale@gmail.com>
2022-04-11 15:42:00 +0200

    Fixed color appbar

--- kipos/lib/screens/homePage.dart
+++ kipos/lib/screens/homePage.dart
@@ -20,7 +20,7 @@ class HomePage extends StatelessWidget {
                 color: Color.fromARGB(255, 255, 255, 255),
                 fontSize: 26,
                 fontWeight: FontWeight.bold))),
-        backgroundColor: Color.fromARGB(213, 43, 184, 43),
+        backgroundColor: Color.fromARGB(213, 34, 175, 34),
         leading: IconButton(
           icon: const Icon(
             lightbulb,
