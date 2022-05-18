d1ffb92c1bb58047ac5873ad1beab1ffe6719f9e
ireneztt <irene.zanettin@studenti.unipd.it>
2022-05-18 12:47:00 +0200

    modify logout page

--- kipos/lib/screens/logoutPage.dart
+++ kipos/lib/screens/logoutPage.dart
@@ -1,8 +1,118 @@
 import 'package:flutter/material.dart';
 import 'package:kipos/screens/homePage.dart';
 import 'package:kipos/screens/loginPage.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class LogoutPage extends StatelessWidget {
+  const LogoutPage({Key? key}) : super(key: key);
+
+  static const route = '/logout/';
+  static const routename = 'logoutPage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton2(
+            customButton: const Icon(
+              Icons.list,
+              size: 46,
+              color: Colors.red,
+            ),
+            customItemsIndexes: const [3],
+            customItemsHeight: 8,
+            items: [
+              ...MenuItems.firstItems.map(
+                (item) => DropdownMenuItem<MenuItem>(
+                  value: item,
+                  child: MenuItems.buildItem(item),
+                ),
+              ),
+              const DropdownMenuItem<Divider>(enabled: false, child: Divider()),
+              ...MenuItems.secondItems.map(
+                (item) => DropdownMenuItem<MenuItem>(
+                  value: item,
+                  child: MenuItems.buildItem(item),
+                ),
+              ),
+            ],
+            onChanged: (value) {
+              MenuItems.onChanged(context, value as MenuItem);
+            },
+            itemHeight: 48,
+            itemPadding: const EdgeInsets.only(left: 16, right: 16),
+            dropdownWidth: 160,
+            dropdownPadding: const EdgeInsets.symmetric(vertical: 6),
+            dropdownDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.redAccent,
+            ),
+            dropdownElevation: 8,
+            offset: const Offset(0, 8),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuItem {
+  final String text;
+  final IconData icon;
+
+  const MenuItem({
+    required this.text,
+    required this.icon,
+  });
+}
+
+class MenuItems {
+  static const List<MenuItem> firstItems = [home, share, settings];
+  static const List<MenuItem> secondItems = [logout];
+
+  static const home = MenuItem(text: 'Home', icon: Icons.home);
+  static const share = MenuItem(text: 'Share', icon: Icons.share);
+  static const settings = MenuItem(text: 'Settings', icon: Icons.settings);
+  static const logout = MenuItem(text: 'Log Out', icon: Icons.logout);
+
+  static Widget buildItem(MenuItem item) {
+    return Row(
+      children: [
+        Icon(item.icon, color: Colors.white, size: 22),
+        const SizedBox(
+          width: 10,
+        ),
+        Text(
+          item.text,
+          style: const TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  static onChanged(BuildContext context, MenuItem item) {
+    switch (item) {
+      case MenuItems.home:
+        //Do something
+        break;
+      case MenuItems.settings:
+        //Do something
+        break;
+      case MenuItems.share:
+        //Do something
+        break;
+      case MenuItems.logout:
+        //Do something
+        break;
+    }
+  }
+}  
+    
+    
+/*class LogoutPage extends StatelessWidget {
   const LogoutPage({Key? key}) : super(key: key);
 
   static const route = '/logout/';
@@ -31,4 +141,4 @@ class LogoutPage extends StatelessWidget {
       ],
     );
   }
-}
+}*/
